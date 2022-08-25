6700a99239ff08f4655c4104c243691a626e5617
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-08-25 23:33:40 +0300

    Update function.js

--- function.js
+++ function.js
@@ -8,3 +8,24 @@ function sum(a, b){
 
 sum (a,b)
 sum(3,7)
+
+console.dir(sum)
+
+function myFn() {}
+
+myFn()
+
+const personOne = {
+  name: 'name1',
+  age: 21
+}
+
+function increasePersonAge(person) {
+  const updatedPerson = Object.assign({}, person)
+  updatedPerson.age += 1
+  return updatedPerson
+}
+
+const updatedPersonOne = increasePersonAge(personOne)
+console.log(personOne.age)
+console.log(updatedPersonOne.age)
