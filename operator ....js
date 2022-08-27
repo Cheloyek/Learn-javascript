eba51006f7f86154ad9fef8c497b7e4e90ab6559
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-08-28 02:17:23 +0300

    Update operator ....js

--- operator ....js
+++ operator ....js
@@ -17,10 +17,16 @@ const bButton = {
 console.log(button) // не меняется
 console.log(blueButton) // свойства объекта button и добавление новых
 console.table(blueButton) // выводит свойства объекта в виде таблицы
-console.log(bButton)
+console.log(bButton)//{ button: { width: 200, text: 'Buy' }, color: 'b' }
 
 const greenButton = {
   ...blueButton, 
   color: 'green' // у объекта blueButton уже есть свойство color, перезапишет его значение
 }
-console.log(greenButton)
+console.log(greenButton)//{ width: 200, text: 'Buy', color: 'green' }
+
+const yellowButton = {
+  color: 'yellow', // значение blue не перезапишется на значение yellow
+  ...blueButton // значение желтый будет перезаписано на blue
+}
+console.log(yellowButton) //{ color: 'blue', width: 200, text: 'Buy' }
