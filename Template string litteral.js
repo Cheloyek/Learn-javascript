4f5bbb803df31065f5fa97bb8031ac570cb09588
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-08-28 02:41:27 +0300

    Update Template string litteral.js

--- Template string litteral.js
+++ Template string litteral.js
@@ -3,7 +3,7 @@ const Template = 'Template'
 const string = 'string'
 const litteral ='litteral'
 const nottsl = notTemplate + ' ' + string + ' ' + litteral
-const tsl = `${Template} ${string} ${litteral}`
+const tsl = `${Template} ${string} ${litteral}` //позволяет на месте переменных использовать функции
 
 console.log(nottsl)
 console.log(tsl)
