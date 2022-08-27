1e18213f1c8c74b020c7024dd3249fdb6eca67a1
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-08-28 02:40:10 +0300

    Add files via upload

---
+++ Template string litteral.js
@@ -0,0 +1,9 @@
+const notTemplate = 'notTemplate'
+const Template = 'Template' 
+const string = 'string'
+const litteral ='litteral'
+const nottsl = notTemplate + ' ' + string + ' ' + litteral
+const tsl = `${Template} ${string} ${litteral}`
+
+console.log(nottsl)
+console.log(tsl)
