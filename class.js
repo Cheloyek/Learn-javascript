cc9ab6afea01a3fa39605a9a87718e8f0d965c64
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-08-31 23:15:25 +0300

    Create class.js

---
+++ class.js
@@ -0,0 +1,15 @@
+class Comment {
+  // constructor() - вызывается когда необходимо создать новый экземпляр класса
+  constructor(text) {
+    this.text = text
+    this.votesQty = 0 //начальное количество голосов
+  }
+  //upvote() - наследуется каждым экземпляром класса
+  upvote() {
+    this.votesQty += 1 //функция, увеличивающая количество голосов на 1
+  }
+}
+
+
+// Создание экземпляра при помощи - new
+const firstComment = new Comment('First comment') // Comment - вызывает функцию constructor, которая создает новый экземпляр, наследующий все методы
