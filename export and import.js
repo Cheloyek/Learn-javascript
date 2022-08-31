9e46092553f825abb5cc2daca8ee3d3ce87fbeeb
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-08-31 22:22:59 +0300

    Update export and import.js

--- export and import.js
+++ export and import.js
@@ -6,6 +6,9 @@ export default sum // экспортирует
 
 //file 2
 import sum from './file 1' //путь к файлу из которого импортируем
+/* можно импортировать но с другим названием функции
+import sumNumbers from './file 1'
+*/
 
 const res = sum(1, 1)
 console.log (res)
