29ecc6a7c956c017fe59fa3581c70fbf7660cd43
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-08-31 22:35:20 +0300

    Update export and import.js

--- export and import.js
+++ export and import.js
@@ -9,9 +9,28 @@ const c = 1
 //file 2
 import sum from './file 1' //путь к файлу из которого импортируем
 /* можно импортировать, но с другим названием функции
-import sumNumbers from './file 1'
+import sumNumbers from './file 1.js'
 */
 
 const res = sum(1, 1)
 console.log (res) // выведет сумму 2
 console.log (c) // выдаст ошибку, импортировали только sum
+
+//Пример экспорта нескольких переменных
+//file 1
+const one = 1
+const two = 'two'
+
+export {
+one,
+two
+}
+
+//file 2
+import {
+  one, // имена переменных должны совпадать
+  two
+} from './file 1.js'
+
+console.log(one) // 1
+console/log(two) // 'two'
