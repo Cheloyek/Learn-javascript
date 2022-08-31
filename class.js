f6ba80b4fbc11288852597612db5004af0491ab5
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-01 00:09:17 +0300

    Update class.js

--- class.js
+++ class.js
@@ -14,4 +14,7 @@ class Comment {
 // Создание экземпляра при помощи - new
 const firstComment = new Comment('First comment') // Comment - вызывает функцию constructor, которая создает новый экземпляр, наследующий все методы
 //Comment.prototype.constructor === Comment
-// при применении firstComment.upvote() значение votesQty увеличится на 1, тк вызвали однократно. При следующем вызове firstComment.upvote() значение votesQty увеличится
+// при вызове firstComment.upvote() значение votesQty увеличится на 1, тк вызвали однократно. При следующем вызове firstComment.upvote() значение votesQty увеличится
+const secondComment = new Comment ('Second comment') // создание второго экземпляра
+//при вызове secondComment.upvote() значение votesQty увеличится на 1 для этого экземпляра
+//secondComment.votesQty - покажет количество вызовов
