4b8912893d0ada9de2ac63d66c13785e237a2e15
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-04 22:52:19 +0300

    Update arrow function.js

--- arrow function.js
+++ arrow function.js
@@ -5,3 +5,10 @@ let func = function(arg1, arg2, ...argN) {
 
 //стрелочная функция
 let func = (arg1, arg2, ...argN) => expression;
+
+
+//стрелочная функция без аргумента
+let sayHi = () => alert("Hello!");
+
+sayHi();
+
