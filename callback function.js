306534a8736256de9f7188e20abe2f3bd0723b7b
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-04 22:39:00 +0300

    Create callback function.js

---
+++ callback function.js
@@ -0,0 +1,29 @@
+//синтаксис ask(question, yes, no)
+//variant 1
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+function showOk() {
+  alert( "Вы согласны." );
+}
+
+function showCancel() {
+  alert( "Вы отменили выполнение." );
+}
+
+// использование: функции showOk, showCancel передаются в качестве аргументов ask
+ask("Вы согласны?", showOk, showCancel);
+
+//variant 2
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+);
