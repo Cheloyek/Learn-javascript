97affafe3922df389e37f0b5d327a323fa663d5b
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-07 09:35:14 +0300

    Create Числа.js

---
+++ Числа.js
@@ -0,0 +1,5 @@
+//метод toString(base) - возвращает строковое представление числа num в системе счисления base.
+let num = 255;
+
+alert( num.toString(16) );  // ff
+alert( num.toString(2) );   // 11111111
