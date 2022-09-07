04e9c95b15a4095fe7b4c4733b47b3de7e61af2c
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-07 18:44:28 +0300

    Create sum array.js

---
+++ sum array.js
@@ -0,0 +1,26 @@
+//Просит пользователя ввести значения, используя prompt и сохраняет их в массив.
+//Заканчивает запрашивать значения, когда пользователь введёт не числовое значение, пустую строку или нажмёт «Отмена».
+//Подсчитывает и возвращает сумму элементов массива.
+
+function sumInput() {
+
+  let numbers = [];
+
+  while (true) {
+
+    let value = prompt("Введите число", 0);
+
+    // Прекращаем ввод?
+    if (value === "" || value === null || !isFinite(value)) break;
+
+    numbers.push(+value);
+  }
+
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+alert( sumInput() );
