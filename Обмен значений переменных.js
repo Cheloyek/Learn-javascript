217904ca14e6cc9e1995d4372724ea3b21b706bd
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-09-08 16:18:37 +0300

    Create Обмен значений переменных.js

---
+++ Обмен значений переменных.js
@@ -0,0 +1,7 @@
+let guest = "Jane";
+let admin = "Pete";
+
+// Давайте поменяем местами значения: сделаем guest = "Pete", а admin = "Jane"
+[guest, admin] = [admin, guest];
+
+alert(`${guest} ${admin}`); // Pete Jane
